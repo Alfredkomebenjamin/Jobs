6caad428c28534700b06089321f9fe0609e418f7
Alfredkomebenjamin <alfredkomebenjamin@gmail.com>
2019-02-11 11:32:27 +0100

    Created new files

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width">
+<meta name="description" content="Affordable and proffessional web design">
+<meta name="keywords" content="web design, affordable web design, proffessional web design">
+<meta name = "author" content= "Kome">
+<title>Acme web design | Welcome</title>
+<link rel="stylesheet" href="css/my_style.css">
+</head>
+<body>
+<div class="my_space">
+<h1>Welcome to Blog</h1>
+</div>
+
+<header>
+
+
+<div id="container">
+<div id="branding">
+	<h1><span class="highlight">BLog</span> Inside</h1>
+</div>	
+<nav>
+<ul>
+<li class="current"><a href="index.html">Home</a></li>
+<li><a href="About/about_us.html">About us</a></li>
+<li><a href="Services/services.html">Services</a></li>
+</ul>	
+</nav>
+</div>		 
+</header>
+
+<section id="showcase"> 
+<div class ="container">
+<h1> Affordable preffessional web design</h1>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic totam, debitis nemo unde.</p>
+</div>
+</section>
+	
+<section id="newsletter">
+<div class="container">	<h1>
+Subscribe to our Newsletter
+</h1>
+<form>
+<input type="email" placeholder="Please Enter Your Email....">
+<button type="submit" class="button_1">Subscribe</button>
+</form>
+</div>
+</section>
+
+<section id="boxes">
+<div class="container">
+<div class="box">
+<img src="./images/HTML5.jpg" alt="HTML5">
+<h3> HTML5 Markup</h3>
+<p>`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis ad.!</p></div>
+<div class="box">
+<img src="./images/CSS3.jpg" alt="CSS3">
+<h3>CSS3 STYLING</h3>
+<p>`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis ad.</p></div>
+<div class="box">
+<img src="./images/GRAPHICS.jpg" alt="GRAPHICS DESIGN">
+<h3>GRAPHIC DESIGN</h3>
+<p>`Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis ad.</p>
+</div>
+</div>
+</section>
+<footer>
+<p>Acme Web Design, copyright &copy; 2019</p>	
+</footer>
+</body>
+</html>
